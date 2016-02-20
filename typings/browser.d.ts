43a9bca085d6706cb0844963398391ccb7d594fa
Viliam Elischer <viliam.elischer@zoomint.com>
2016-02-20 18:09:42 +0100

    Removed typings + moved to postinstall

--- typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/react-dom/react-dom.d.ts" />
-/// <reference path="browser/ambient/react/react.d.ts" />
-/// <reference path="browser/definitions/react-redux/react-redux.d.ts" />
-/// <reference path="browser/definitions/redux/redux.d.ts" />
